90a2c0fcb586fd04bd9dc809c98c2acea82fecac
bojo992 <93447305+zed992@users.noreply.github.com>
2024-10-05 15:42:01 +0100

    Upload default springboot

---
+++ project/src/main/java/com/cs4337/project/ProjectApplication.java
@@ -0,0 +1,23 @@
+package com.cs4337.project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
+@RestController
+@SpringBootApplication
+public class ProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectApplication.class, args);
+	}
+
+	@GetMapping("/test")
+	public Map<String, Object> sayHello() {
+		return Map.of("message", "Hello World!");
+	}
+
+}
