06362f1799d1f4e8a2d8f1a66cc36936a22fa4b0
Bojo992 <mm22mm44bbi@gmail.com>
2024-12-04 12:22:51 +0000

    Initial implementation of s3 integration

---
+++ MediaStorage/src/test/java/com/cs4337/mediastorage/MediaStorageApplicationTests.java
@@ -0,0 +1,17 @@
+package com.cs4337.mediastorage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+
+@SpringBootTest
+class MediaStorageApplicationTests {
+
+
+
+    @Test
+    void contextLoads() {
+    }
+
+}
