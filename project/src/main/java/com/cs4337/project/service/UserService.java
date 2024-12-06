9af7879ff78e3ef9249fdea30929dd8661d49826
Bojo992 <mm22mm44bbi@gmail.com>
2024-12-06 21:49:01 +0000

    Integration of db into main project

--- project/src/main/java/com/cs4337/project/service/UserService.java
+++ project/src/main/java/com/cs4337/project/service/UserService.java
@@ -1,25 +1,52 @@
 package com.cs4337.project.service;
 
-import com.cs4337.project.repository.UserRepository;
+import com.cs4337.project.model.Users;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.stereotype.Service;
 
+import java.net.URI;
+import java.net.URLEncoder;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.nio.charset.StandardCharsets;
 import java.util.Map;
+import java.util.Optional;
 
 @Service
 public class UserService {
-    private UserRepository userRepository;
 
-    public UserService(UserRepository userRepository) {
-        this.userRepository = userRepository;
-    }
+    ObjectMapper objectMapper = new ObjectMapper();
+    String baseUrl = "ec2-52-212-119-177.eu-west-1.compute.amazonaws.com:8082";
 
     public Map<String, Integer> getUser(String username) {
-        var user = userRepository.getByUsername(username);
+        try {
+            HttpClient client = HttpClient.newHttpClient();
+            String param2 = URLEncoder.encode(username, StandardCharsets.UTF_8);
 
-        if (user.isPresent()) {
-            return Map.of("userId", user.get().getId());
+            HttpRequest request1 = HttpRequest.newBuilder()
+                    .uri(new URI(baseUrl + "/users?email=" + param2))
+                    .GET()
+                    .build();
+
+            // Send the request and get the response
+            HttpResponse<String> response = client.send(request1, HttpResponse.BodyHandlers.ofString());
+
+            Optional<Users> user = Optional.of(null);
+
+            if (response.statusCode() == 200) {
+                var temp = objectMapper.readValue(response.body(), Users.class);
+
+                user = Optional.of(temp);
+            }
+
+            if (user.isPresent()) {
+                return Map.of("userId", user.get().getId());
+            }
+
+            return Map.of("userId", 0);
+        } catch (Exception e) {
+            return Map.of("userId", 0);
         }
-
-        return Map.of("userId", 0);
     }
 }
